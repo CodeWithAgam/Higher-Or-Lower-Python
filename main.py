f9c0969d0bce774cdb79985a76223c1d9e98721d
Agamdeep Singh <agamdeep579@gmail.com>
2022-03-17 00:40:58 +0530

    Updated the code

---
+++ main.py
@@ -0,0 +1,9 @@
+# Created by Agamdeep Singh / CodeWithAgam
+# Youtube: CodeWithAgam
+# Github: CodeWithAgam
+# Instagram: @agamdeep_21, @coderagam001
+# Twitter: @CoderAgam001
+# Linkdin: Agamdeep Singh
+
+from art import *
+from game_data import data
