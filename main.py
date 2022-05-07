237212642eb619047a1afc4ac45d07bad4fb5ad6
Agamdeep Singh <agamdeep579@gmail.com>
2022-05-07 22:56:03 +0530

    Updated the code

--- main.py
+++ main.py
@@ -5,55 +5,61 @@
 # Twitter: @CoderAgam001
 # Linkdin: Agamdeep Singh
 
-from art import *
+from os import system, name
+from art import vs, logo
 from game_data import data
 from random import randint
 
-print(logo)
+
+# Function to clear the screen
+def clear():
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
 
 def generate_data():
+    """Generate the game data"""
     random_number = randint(0, len(data) - 1)
     name = data[random_number]["name"]
     followers = data[random_number]["followers"]
     return name, followers
 
-first = generate_data()
+def compare():
+    """Check if a guess is correct and return the guess"""        
+    if first[1] > second[1]:
+        if guess == "a":
+            return True
+
+        else:
+            return False
+
+print(logo)
+score = 0
 second = generate_data()
 
-print(f"Comapre the follower count of the following people:\n")
-
-def compare():
-    if first[1] > second[1]:
-        return 1
-    
-    elif first[1] < second[1]:
-        return 2
-
-print(f"A: {first[0]}")
-print(vs)
-print(f"B: {second[0]}")
-
-def main():
-    selected = input("\nSelect A or B: ").lower()
-    result = compare() 
-
-    end = False
-    while not end:
-        if selected == "a" and result == 1:
-            return "Correct!"
-
-        elif selected == "b" and result == 2:
-            return "Correct!"
-
-        elif selected == "a" and result == 2:
-            print("Wrong!")
-            end = True
-
-        elif selected == "b" and result == 1:
-            print("Wrong!")
-            end = True
-
+end = False
+while not end:
+    # Extract data from game_data.py
+    first = second
+    second = generate_data()
+    while first == second:
         second = generate_data()
-        compare()
 
-main()
+    # Print the data
+    print(f"A - {first[0]}")
+    print(vs)
+    print(f"B - {second[0]}")
+    guess = input("\nWho Has More Instagram Followers? Enter A or B: ").lower()
+    
+    clear()
+    print(logo)
+
+    returned = compare()
+    if returned == True:
+        score += 1
+        print("\nYou Guessed It Correctly! Your current score is {}".format(score))
+
+    else:
+        end = False
+        print("You got it wrong! Your Final Score is {}".format(score))
